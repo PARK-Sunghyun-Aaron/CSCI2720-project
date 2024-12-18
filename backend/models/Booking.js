551aef3cdedde04848f90e8b42e6abccb768b1a9
BasilLowe <122352565+BasilLowe@users.noreply.github.com>
2024-12-18 22:00:02 +0800

    Update Booking.js

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,3 +1,12 @@
+/*I/We declare that the lab work here submitted is original except for source material explicitly acknowledged,
+and that the same or closely related material has not been previously submitted for another course. 
+I/We also acknowledge that I/We am aware of University policy and regulations on honesty in academic work, 
+and of the disciplinary guidelines and procedures applicable to breaches of such policy and regulations, 
+as contained in the website. University Guideline on Academic Honesty: https://www.cuhk.edu.hk/policy/academichonesty/ 
+Student Name : LIN Yi Student ID : 1155232784 Student Name : MANAV Suryansh Student ID : 1155156662 
+Student Name : MUI Ka Shun Student ID : 1155194765 Student Name : PARK Sunghyun Student ID : 1155167933 
+Student Name : RAO Penghao Student ID : 1155191490 Class/Section : CSCI2720 Date : 2024-12-04
+*/
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
